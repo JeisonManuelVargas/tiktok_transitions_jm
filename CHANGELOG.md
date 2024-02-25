f71c75fe0fbe09ebf3d8c2042d4a208e926ebd31
jeison vargas <jeisonvargas@flutterlabapps.com>
2024-02-25 10:37:16 -0500

    implemented README.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 ## 0.0.1
 - initial release.
+
+## 0.0.2
+- exported TikTokManagerBuilder typedef
+
--- lib/tiktok_transitions_jm.dart
+++ lib/tiktok_transitions_jm.dart
@@ -5,3 +5,4 @@ export 'src/feature/tiktok_transitions_jm.dart';
 
 //Models
 export 'src/utils/model/tik_tok_model.dart';
+export 'src/utils/model/tik_tok_manager_builder.dart';
